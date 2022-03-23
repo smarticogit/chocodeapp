1bc9a83ef25db8cd7852068cf99e3841e4790497
daniel.silveira <daniel.repres@hotmail.com>
2022-03-23 19:27:13 -0300

    Alteracao dos endpoints

--- ifood/src/main/java/br/com/chocode/ifood/controller/LoginController.java
+++ ifood/src/main/java/br/com/chocode/ifood/controller/LoginController.java
@@ -1,13 +1,10 @@
 package br.com.chocode.ifood.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import br.com.chocode.ifood.services.ILoginService;
 
 @RestController
 @CrossOrigin("*")
@@ -17,9 +14,10 @@ public class LoginController {
 //	private ILoginService service;
 	
 	@PostMapping("/login")
-	public ResponseEntity<String> login(@RequestBody String email, @RequestBody String senha){
-		
-		return ResponseEntity.status(200).body("Acelera!");
+	public ResponseEntity<String> login(@RequestBody String email,String senha){
+		System.out.printf("Front email -> ", email);
+		System.out.printf("Front senha -> ", senha);
+		return ResponseEntity.status(200).body("Mete Marcha");
 	}
 	
 	
--- ifood/src/main/java/br/com/chocode/ifood/controller/PedidoController.java
+++ ifood/src/main/java/br/com/chocode/ifood/controller/PedidoController.java
@@ -7,9 +7,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.chocode.ifood.dto.PedidoDTO;
@@ -17,21 +17,34 @@ import br.com.chocode.ifood.services.IPedidoService;
 
 @RestController
 @CrossOrigin("*")
-@RequestMapping("/pedido")
 public class PedidoController {
 
 	@Autowired
 	private IPedidoService service;
 
-	@GetMapping("/findall")
+	@GetMapping("/pedidos")
 	public ResponseEntity<List<PedidoDTO>> findAll() {
 		return ResponseEntity.ok(service.findAll());
 	}
 
-	@PutMapping("/status/{id}")
+	@PutMapping("/pedido/{id}/status/")
 	public ResponseEntity<PedidoDTO> update(@PathVariable Long id, @RequestBody String status) {
 		return ResponseEntity.ok(service.update(id, status));
 	}
 	
+	@GetMapping("/pedido/{id}/geolocalizacao")
+	public String texto () {
+		return "ok";
+	}
+	
+	@PostMapping("/pedido/{id}/geolocalizacao")
+	public String texto2 () {
+		return "ok";
+	}
+	
+	@PostMapping("/pedido/{id_pedido}/entregador/{id_entregador}")
+	public String texto3 () {
+		return "ok";
+	}
 	
 }
